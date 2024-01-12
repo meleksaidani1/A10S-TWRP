af1f5ff9d9513b34b47762faba91566d3681d1d4
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-12 12:39:46 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,11 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Define lunch combos using COMMON_LUNCH_CHOICES
+# Uncomment the following line to include the common lunch choices
+source build/envsetup.sh
 
+# Uncomment the following line to use the COMMON_LUNCH_CHOICES variable
 lunch_menu_show_common_choices=true
 
+# Comment out the add_lunch_combo lines
+# add_lunch_combo omni_a10s-user
+# add_lunch_combo omni_a10s-userdebug
+# add_lunch_combo omni_a10s-eng
+
+# Define lunch combos using COMMON_LUNCH_CHOICES
 COMMON_LUNCH_CHOICES := \
-add_lunch_combo  omni_a10s-user \
-add_lunch_combo omni  omni_a10s-userdebug \
-add_lunch_combo  omni_a10s-eng
+  omni_a10s-user \
+  omni_a10s-userdebug \
+  omni_a10s-eng
+
+# Add lunch combos from COMMON_LUNCH_CHOICES
+for combo in $COMMON_LUNCH_CHOICES; do
+    add_lunch_combo $combo
+done
