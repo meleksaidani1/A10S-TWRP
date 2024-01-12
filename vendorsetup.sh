a20700331e93726056bcebb5542affe5f41977c2
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-01-12 11:44:43 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,12 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a10s-user
-add_lunch_combo omni_a10s-userdebug
-add_lunch_combo omni_a10s-eng
+# Define lunch combos using COMMON_LUNCH_CHOICES
+
+COMMON_LUNCH_CHOICES := \
+  omni_a10s-user \
+  omni_a10s-userdebug \
+  omni_a10s-eng
